5b62c5cc07f5ef26f5f9b33e7a45b6a6830029f3
Jorge Rodriguez-Moreno <rdz.invent@gmail.com>
2025-10-17 22:59:06 -0600

    Finally make run_qemu to success in Docker

--- deployment/qemu/scripts/run_qemu.sh
+++ deployment/qemu/scripts/run_qemu.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
+
+DOCKER_ENV="/.dockerenv"
+
+if [ -e "$DOCKER_ENV" ]; then
+  KERNEL_IMAGE="/var/jenkins_home/workspace/simtemp-system/deployment/qemu/build/arch/arm/boot/zImage"
+  DTB_FILE="/var/jenkins_home/workspace/simtemp-system/deployment/qemu/build/arch/arm/boot/dts/imx6q-sabrelite.dtb"
+  ROOTFS_IMAGE="/var/jenkins_home/workspace/simtemp-system/deployment/qemu/rootfs.cpio.gz"
+else
+  KERNEL_IMAGE="linux-imx-5.10/arch/arm/boot/zImage"
+  DTB_FILE="linux-imx-5.10/arch/arm/boot/dts/imx6q-sabrelite.dtb"
+  ROOTFS_IMAGE="rootfs.cpio.gz"
+fi
+
 qemu-system-arm -M sabrelite \
                 -cpu cortex-a9 \
                 -m 1024 -nographic -no-reboot \
-                -kernel linux-imx-5.10.72/arch/arm/boot/zImage \
-                -dtb linux-imx-5.10.72/arch/arm/boot/dts/imx6q-sabresd.dtb \
-                -initrd rootfs.cpio.gz \
+                -kernel $KERNEL_IMAGE \
+                -dtb $DTB_FILE \
+                -initrd $ROOTFS_IMAGE \
                 -append "console=ttymxc0,115200 earlycon=imx,0x021e8000,115200 rdinit=/init loglevel=8" \
                 -monitor telnet:127.0.0.1:45454,server,nowait
