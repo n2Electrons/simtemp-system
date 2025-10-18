09b50b756f5291afcda27042199ec898a7b34993
Jorge Rodriguez-Moreno <rdz.invent@gmail.com>
2025-10-17 23:20:15 -0600

    NOT THE SAME! build/.../zImage diff than linux-imx-5.10/.../zImage
    linux-imx-5.10/.../zImage WORKS FINE!

--- deployment/qemu/scripts/run_qemu.sh
+++ deployment/qemu/scripts/run_qemu.sh
@@ -3,14 +3,19 @@
 DOCKER_ENV="/.dockerenv"
 
 if [ -e "$DOCKER_ENV" ]; then
-  KERNEL_IMAGE="/var/jenkins_home/workspace/simtemp-system/deployment/qemu/build/arch/arm/boot/zImage"
-  DTB_FILE="/var/jenkins_home/workspace/simtemp-system/deployment/qemu/build/arch/arm/boot/dts/imx6q-sabresd.dtb"
+  KERNEL_IMAGE="/var/jenkins_home/workspace/simtemp-system/deployment/qemu/linux-imx-5.10/arch/arm/boot/zImage"
+  DTB_FILE="/var/jenkins_home/workspace/simtemp-system/deployment/qemu/linux-imx-5.10/arch/arm/boot/dts/imx6q-sabresd.dtb"
   ROOTFS_IMAGE="/var/jenkins_home/workspace/simtemp-system/deployment/qemu/rootfs.cpio.gz"
 else
   KERNEL_IMAGE="linux-imx-5.10/arch/arm/boot/zImage"
   DTB_FILE="linux-imx-5.10/arch/arm/boot/dts/imx6q-sabresd.dtb"
   ROOTFS_IMAGE="rootfs.cpio.gz"
 fi
+
+echo "Current working directory: $(pwd)"
+echo "KERNEL_IMAGE env var: '$KERNEL_IMAGE'"
+echo "DTB_FILE env var: '$DTB_FILE'"
+echo "ROOTFS_IMAGE env var: '$ROOTFS_IMAGE'"
 
 qemu-system-arm -M sabrelite \
                 -cpu cortex-a9 \
