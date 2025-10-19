8420b8a75c2a679a8ef761c91b24cc042622c8ad
Jorge Rodriguez-Moreno <rdz.invent@gmail.com>
2025-10-18 18:25:47 -0600

    Added -serial stdio to make a proper terminal en Qemu

--- deployment/qemu/scripts/run_qemu.sh
+++ deployment/qemu/scripts/run_qemu.sh
@@ -17,18 +17,34 @@ echo "KERNEL_IMAGE env var: '$KERNEL_IMAGE'"
 echo "DTB_FILE env var: '$DTB_FILE'"
 echo "ROOTFS_IMAGE env var: '$ROOTFS_IMAGE'"
 
-qemu-system-arm -M sabrelite \
-                -cpu cortex-a9 \
-                -m 1024 -nographic -no-reboot \
-                -kernel $KERNEL_IMAGE \
-                -dtb $DTB_FILE \
-                -initrd $ROOTFS_IMAGE \
-                -append "console=ttymxc0,115200 earlycon=imx,0x021e8000,115200 rdinit=/init \
-                quiet loglevel=8 initcall_debug printk.time=1 \
-                modprobe.blacklist=mxc_v4l2_output,imx-ipuv3,imx6q-vdoa,imx-vpu,imxdrm,imx-hdmi, \
-                galcore,max8903_driver,max8903,max11801_ts,max11801,pfuze100,pfuze100-regulator, \
-                max8903_charger,max8903-charger,pfuze100_regulator,pfuze-regulator" \
-#                -monitor telnet:127.0.0.1:45455,server,nowait
+# Configure QEMU parameters based on environment
+if [ -e "$DOCKER_ENV" ]; then
+  # Docker environment - optimize for CI/CD
+  echo "Running in Docker environment"
+  qemu-system-arm -M sabrelite \
+                  -cpu cortex-a9 \
+                  -m 1024 -nographic -no-reboot \
+                  -serial stdio \
+                  -kernel $KERNEL_IMAGE \
+                  -dtb $DTB_FILE \
+                  -initrd $ROOTFS_IMAGE \
+                  -append "console=ttymxc0,115200 earlycon=imx,0x021e8000,115200 rdinit=/init quiet loglevel=8 initcall_debug printk.time=1 modprobe.blacklist=mxc_v4l2_output,imx-ipuv3,imx6q-vdoa,imx-vpu,imxdrm,imx-hdmi,galcore,max8903_driver,max8903,max11801_ts,max11801,pfuze100,pfuze100-regulator,max8903_charger,max8903-charger,pfuze100_regulator,pfuze-regulator,max8903_driver_init"
+else
+  # Local environment - enable monitor for debugging
+  echo "Running in local environment"
+  qemu-system-arm -M sabrelite \
+                  -cpu cortex-a9 \
+                  -m 1024 -nographic -no-reboot \
+                  -kernel $KERNEL_IMAGE \
+                  -dtb $DTB_FILE \
+                  -initrd $ROOTFS_IMAGE \
+                  -append "console=ttymxc0,115200 earlycon=imx,0x021e8000,115200 rdinit=/init quiet loglevel=8 initcall_debug printk.time=1 modprobe.blacklist=mxc_v4l2_output,imx-ipuv3,imx6q-vdoa,imx-vpu,imxdrm,imx-hdmi,galcore,max8903_driver,max8903,max11801_ts,max11801,pfuze100,pfuze100-regulator,max8903_charger,max8903-charger,pfuze100_regulator,pfuze-regulator,max8903_driver_init" \
+                  -monitor telnet:127.0.0.1:45455,server,nowait
+fi
+
+#                modprobe.blacklist=mxc_v4l2_output,imx-ipuv3,imx6q-vdoa,imx-vpu,imxdrm,imx-hdmi, \
+#                galcore,max8903_driver,max8903,max11801_ts,max11801,pfuze100,pfuze100-regulator, \
+#                max8903_charger,max8903-charger,pfuze100_regulator,pfuze-regulator" \
 
 # To avoid annoying messages, need to unbind
 # echo 1-0048 > /sys/bus/i2c/drivers/max11801_ts/unbind
